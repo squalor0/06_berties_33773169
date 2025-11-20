2134ae109f3475972bd1b5e4c2f4b67e8fdc75c2
Blake Murphy <bmurp001@gold.ac.uk>
2025-11-20 08:48:29 +0000

    updated gold, smiths login

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, firstName, lastName, email, hashedPassword)
+VALUES ('gold', 'Gold', 'User', 'gold@example.com', '$2b$10$o8985WgA6APH/5B.bEeaR.NCmiojhp.vkQmkjUIq.yS/zglhqLWQS');
