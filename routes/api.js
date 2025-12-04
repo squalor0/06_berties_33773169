b906a37be750439c5cc838a342c8fe02c481d3e0
Blake Murphy <bmurp001@gold.ac.uk>
2025-12-04 11:15:30 +0000

    added api functions

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,51 @@
+// routes/api.js
+
 const express = require("express");
 const router = express.Router();
 
 router.get('/books', function (req, res, next) {
+    let sqlquery = "SELECT * FROM books";
+    let conditions = [];
+    let params = [];
 
-    // Query database to get all the books
-    let sqlquery = "SELECT * FROM books"
+    // --- Search by name ---
+    if (req.query.search) {
+        conditions.push("name LIKE ?");
+        params.push('%' + req.query.search + '%');
+    }
 
-    // Execute the sql query
-    db.query(sqlquery, (err, result) => {
-        // Return results as a JSON object
+    // --- Price range ---
+    if (req.query.minprice) {
+        conditions.push("price >= ?");
+        params.push(req.query.minprice);
+    }
+
+    if (req.query.maxprice) {
+        conditions.push("price <= ?");
+        params.push(req.query.maxprice);
+    }
+
+    // Add WHERE
+    if (conditions.length > 0) {
+        sqlquery += " WHERE " + conditions.join(" AND ");
+    }
+
+    // --- Sorting ---
+    if (req.query.sort === 'name') {
+        sqlquery += " ORDER BY name";
+    } else if (req.query.sort === 'price') {
+        sqlquery += " ORDER BY price";
+    }
+
+    // Execute the SQL query
+    db.query(sqlquery, params, (err, result) => {
         if (err) {
-            res.json(err)
-            next(err)
+            res.json(err);
+            return next(err);
+        } else {
+            res.json(result);
         }
-        else {
-            res.json(result)
-        }
-    })
-})
+    });
+});
 
 module.exports = router;
